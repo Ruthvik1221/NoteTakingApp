eac671c9834616abd2b314c8c4453a9a374dfab2
TANDA RUTHVIK GOUD <truthvikgoud@gmail.com>
2022-12-07 07:49:42 +0530

    Add files via upload

---
+++ server/models/note.js
@@ -0,0 +1,70 @@
+
+const con = require("./db_connect");
+
+// Table Creation 
+async function createTable() {
+  let sql=`CREATE TABLE IF NOT EXISTS notes (
+    userID INT,
+    noteID INT NOT NULL AUTO_INCREMENT,
+    noteContent VARCHAR(255),
+    CONSTRAINT notePK PRIMARY KEY(noteID),
+    CONSTRAINT noteFK FOREIGN KEY(userID) REFERENCES users(userID)
+  ); `
+  await con.query(sql);
+}
+createTable();
+
+// grabbing all notes in database
+async function getAllNotes() {
+  const sql = `SELECT * FROM notes;`;
+  let notes = await con.query(sql);
+  console.log(notes)
+}
+
+
+// Read Note
+async function Read(note) { //content:"hello world"
+  let cNote = await getNote(note); 
+  if(!cNote[0]) throw Error("NoteID not found");
+  return cNote[0];
+}
+
+// Update Note function
+async function editNotes(note) {
+  let sql = `UPDATE notes 
+    SET noteContent = "${note.noteContent}"
+    WHERE userID = ${note.userID}
+  `;
+
+  await con.query(sql);
+  let updatedNote = await getNote(note);
+  return updatedNote[0];
+}
+
+// Delete Note function
+async function deleteNote(note) {
+  let sql = `DELETE FROM notes
+    WHERE userID = ${note.userID}
+  `
+  await con.query(sql);
+}
+
+// Useful Functions
+async function getNote(note) {
+  let sql;
+
+  if(note.userID) {
+    sql = `
+      SELECT * FROM notes
+       WHERE userID = ${note.userID}
+    `
+  } else {
+    sql = `
+    SELECT * FROM notes 
+      WHERE noteID = "${note.noteID}"
+  `;
+  }
+  return await con.query(sql);  
+}
+
+module.exports = { getAllNotes, Read, editNotes, deleteNote};
